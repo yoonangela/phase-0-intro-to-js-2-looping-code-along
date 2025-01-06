b3d71d6fb3126bd8111ea14ab8c303c43f232eee
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-06 14:49:07 +0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const cards = [["Guadalupe", "Ollie", "Aki"], "surprise"];
+function writeCards() {
+  let messages = [];
+  for (let i = 0; i < cards[0].length; i++) {
+    messages.push(`Thank you, ${cards[0][i]}, for the wonderful ${cards[1]} gift!`);;
+  }
+  return messages;
+  
+}
+
+console.log(writeCards());
+
+function countDown(number) {
+
+    while (number>0) {
+        console.log(number);
+        number = number - 1;
+    }
+    console.log(number);
+  }
+
+countDown(10);
